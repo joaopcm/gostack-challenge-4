af6e74a955face0b91f62a18b7f7c63735085e04
Joao Melo <jopcmelo@gmail.com>
2019-10-23 14:37:38 -0300

    chore: Add Post and Comment components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
-import Comment from '../Comment'
+import './styles.css'
 
-export default function Post() {
+function PostHeader({ author, date }) {
   return (
-    <>
-      <h1>Post</h1>
-      <Comment />
-    </>
+    <div className="post-header">
+      <img src={author.avatar} alt="User profile" className="avatar"/>
+      <div className="details">
+        <span>{author.name}</span>
+        <span>{date}</span>
+      </div>
+    </div>
+  )
+}
+
+function PostComments({ comments }) {
+  return (
+    <div className="post-comments">
+      <div className="divider" />
+      {comments.map(comment => (
+        <div key={comment.id} className="comment">
+          <img src={comment.author.avatar} alt="User profile" className="avatar"/>
+          <p>
+            <span>{comment.author.name}</span>
+            {comment.content}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default function Post({ author, date, content, comments }) {
+  return (
+    <div className="post">
+      <PostHeader author={author} date={date} />
+      <p className="post-content">{content}</p>
+      <PostComments comments={comments} />
+    </div>
   );
 }
